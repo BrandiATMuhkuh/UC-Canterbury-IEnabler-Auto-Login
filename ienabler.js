ccdc305db9c5d3a30e0d5c0fb9c3cbb148a55bce
Juergen Brandstetter <juergen@tagaprice.org>
2014-12-06 19:34:23 +1300

    created auto enabler

---
+++ ienabler.js
@@ -0,0 +1,27 @@
+console.log("running ienabler.js");
+
+var casper = require('casper').create();
+var username = casper.cli.get("username");
+var password = casper.cli.get("password");
+
+
+console.log(username, password);
+var links = [];
+
+casper.start('http://ienabler.canterbury.ac.nz:900/', function() {
+    this.fill('form[action="/"]', { DATA: username}, true);
+});
+
+casper.then(function() {
+    this.fill('form[action="/"]', { DATA: password }, true);
+});
+
+casper.then(function() {
+    this.fill('form[action="/"]', {}, true);
+});
+
+casper.run(function() {
+    // echo results in some pretty fashion
+    this.echo('You should be logged in now. ');
+    casper.exit();
+});
