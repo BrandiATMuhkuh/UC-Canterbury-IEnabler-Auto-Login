15c890710445dcb1ec05c51da62f6dd49382c9df
Juergen Brandstetter <juergen@tagaprice.org>
2014-12-06 22:43:50 +1300

    created script to add outlogin to crontab

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "--------------------";
+
+
+mainCommand="casperjs "$PWD"/ienabler.js --username="${1}" --password="${2}
+echo $mainCommand
+
+multibe="0 */6 * * * ""\""$mainCommand"\""
+#echo "$juhu" > "test.txt"
+(crontab -u $USER -l; echo "$multibe" ) | crontab -u $USER -
+
+reboot="@reboot ""\""$mainCommand"\""
+(crontab -u $USER -l; echo "$reboot" ) | crontab -u $USER -
+
+
+
+echo "--------------------";
